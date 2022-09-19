7037b56fd6ab8dcd640582851458e27d54d3970c
Mike Jones <gluemchen81@googlemail.com>
2022-09-19 18:01:51 +0200

    Added Nested Unlisted and Listed containers

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-ReactDOM.
+import ReactDOM from 'react-dom'
+ReactDOM.render(
+    <ul><li>Thing 1</li><li>Thing 2</li></ul>,
+    document.getElementById("root")
+)
